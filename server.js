9003e6d76961b258f050ad7a830650ee6d9bcbb2
dppower <powerdp@gmail.com>
2016-02-04 17:28:12 +0000

    Set up the back-end.

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http');
+var server = http.createServer(app);
+var ws = require('socket.io')(server);
+
+var routes = require('./routes');
+app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), function () {
+    console.log('Server is listening on port ' + app.get('port'));
+})
+
+ws.on('connection', function (socket) {
+    console.log('A user has connected');
+    socket.on('disconnect', function () {
+        console.log('A user has disconnected');
+    })
+})
