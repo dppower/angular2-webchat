5142f6a3f36708f5d40b91cd2c02962d9bd8774d
dppower <powerdp@gmail.com>
2016-02-04 17:50:13 +0000

    Added simple routing to the server.

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 module.exports = router;
